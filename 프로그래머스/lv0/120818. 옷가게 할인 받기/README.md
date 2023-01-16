ec5cc197b7c173621d9e2de135fa6c92116d290e
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-01-16 20:44:46 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/README.md
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 옷가게 할인 받기 - 120818 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120818) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120818#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 10 MB, 시간: 0.00 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,11 +1,5 @@
 def solution(price):
-    answer = price
-    
-    if 100000 <= price < 300000:
-        answer = price * 0.95
-    elif 300000 <= price < 500000:
-        answer = price * 0.9
-    elif price >= 500000:
-        answer = price * 0.8
-        
-    return int(answer)
+    discount_rates = {500000: 0.8, 300000: 0.9, 100000: 0.95, 0: 1}
+    for discount_price, discount_rate in discount_rates.items():
+        if price >= discount_price:
+            return int(price * discount_rate)
