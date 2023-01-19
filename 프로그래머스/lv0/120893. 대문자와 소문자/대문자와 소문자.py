e853c31b774aec40bccdfc91b847ca89acb3c26a
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-01-19 14:37:51 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.17 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,8 +1,2 @@
 def solution(my_string):
-    answer = ""
-    for mystring in my_string:
-        if mystring.isupper():
-            answer += mystring.lower()
-        elif mystring.islower():
-            answer += mystring.upper()
-    return answer
+    return ''.join(i.lower() if i.isupper() else i.upper() for i in my_string)
