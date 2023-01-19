9383ce6e334da68f0ab77021538027df9cef9f92
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-01-19 14:48:50 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,3 +1,3 @@
 def solution(rsp):
-    result = {2:0, 0:5, 5:2}
-    return ''.join(str(result.get(int(i))) for i in rsp)
+    result = {'2':'0', '0':'5', '5':'2'}
+    return ''.join(result.get(i) for i in rsp)
