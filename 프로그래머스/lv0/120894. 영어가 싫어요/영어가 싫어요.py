ba480b1a0e73861123decbfe8f6f28cc91ace884
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-02-14 15:40:35 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,5 @@
+def solution(numbers):
+    zero_to_nine = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+    for k,v in zero_to_nine.items():
+        numbers = numbers.replace(k,v)
+    return int(numbers)
