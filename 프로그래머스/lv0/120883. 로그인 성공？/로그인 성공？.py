cc4dab6d384c3d3236a5925b59b543df1be85198
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-02-16 14:59:07 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,17 +1,7 @@
 def solution(id_pw, db):
-    answer = []
     for id, pw in db:
         if id_pw[0] == id and id_pw[1] == pw:
-            answer.append('login')
+            return 'login'
         elif id_pw[0] == id and id_pw[1] != pw:
-            answer.append('wrong pw')
-        elif id_pw[0] != id:
-            answer.append('fail')
-            
-    if 'login' in answer:
-        return 'login'
-    else:
-        if 'wrong pw' in answer:
             return 'wrong pw'
-        else:
-            return 'fail'
+    return 'fail'
