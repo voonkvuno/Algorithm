67b6f94363d3f1fc9ac01964460d3aa13fe125cc
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-02-23 18:08:46 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,17 +1,4 @@
 def solution(array, n):
-    answer = 0
-    array.append(n)
-    array.sort()
-    idx = array.index(n)
-    if idx == 0:
-        answer = array[idx+1]
-    elif 0 < idx < len(array)-1:
-        if array[idx] - array[idx-1] > array[idx+1] - array[idx]:
-            answer = array[idx+1]
-        elif array[idx] - array[idx-1] < array[idx+1] - array[idx]:
-            answer = array[idx-1]
-        else:
-            answer = array[idx-1]
-    else:
-        answer = array[idx-1]
+    array.sort(key = lambda x : (abs(x-n), x-n))
+    answer = array[0]
     return answer
