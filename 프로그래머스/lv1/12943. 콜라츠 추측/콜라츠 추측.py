2f1b7c8077b21bbe1f5079a0a38b598fcd544931
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-03-02 15:22:14 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,9 @@
+def solution(num):
+    answer = 0
+    while num != 1 and answer <= 500:
+        if num%2 == 0:
+            num /= 2
+        elif num%2 == 1:
+            num = num * 3 + 1
+        answer += 1
+    return answer if answer <= 500 else -1
