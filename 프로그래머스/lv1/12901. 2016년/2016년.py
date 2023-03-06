0cbc7ba0ddc7fb9baeba12ad8f96aa00e2c1d69e
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-03-07 01:07:23 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -1,4 +1,5 @@
 def solution(a, b):
     days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    week = ['FRI','SAT','SUN','MON','TUE','WED','THU']
-    return week[divmod(sum(days[:a]) + b, 7)[1] - 1]
+    week = ['SUN','MON','TUE','WED','THU','FRI','SAT']
+    january1 = 5 # FRI
+    return week[divmod(january1 + sum(days[:a]) + b, 7)[1] - 1]
