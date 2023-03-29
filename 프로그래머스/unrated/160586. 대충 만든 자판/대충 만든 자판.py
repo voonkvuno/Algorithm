897e129054290fdb741ad84e8073bf8bda12f533
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-03-29 17:35:56 +0900

    [unrated] Title: 대충 만든 자판, Time: 15.40 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -13,26 +13,3 @@ def solution(keymap, targets):
             result += min(number) + 1
         answer.append(result)
     return answer
-
-def solution(keymap, targets):
-    answer = []
-    
-    for word in targets:
-        times = 0
-        for char in word:
-            flag = False
-            time = 101
-            for key in keymap:      
-                if char in key:
-                    time = min(key.index(char)+1, time)
-                    flag = True
-                    
-            if not flag:           # 만약 keymap을 전부다 돌았는데도 문자를 찾지 못하면
-                times = -1;break   # 목표 문자열을 작성할 수 없음 
-            
-            times += time          # 하나의 문자에 대해 누른 키를 총합(키)에 더해줌
-            
-        answer.append(times)        
-        # targets에 있는 하나의 단어에 대해 수행이 끝났으면 answer에 누른 키 총합을 넣어줌
-    
-    return answer
