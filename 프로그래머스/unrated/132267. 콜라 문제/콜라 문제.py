6e401848c83d46efb4465c259e6f518daf20048a
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-03-29 17:39:05 +0900

    Delete 프로그래머스/unrated directory

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++
@@ -1,7 +0,0 @@
-def solution(a, b, n):
-    answer = 0
-    while n >= a:
-        coke, reminder = divmod(n,a)
-        answer += coke*b
-        n = coke*b + reminder
-    return answer
--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++
@@ -1,6 +0,0 @@
-def solution(food):
-    answer = ''
-    for i, v in enumerate(food):
-        if i > 0 and v//2 > 0:
-            answer += str(i)*(v//2)
-    return answer + '0' + ''.join(reversed(answer))
