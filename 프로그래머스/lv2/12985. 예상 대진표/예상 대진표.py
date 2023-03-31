92d4a9a35133a35e1455ad9c2483828d80f93b10
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-03-31 17:38:15 +0900

    [level 2] Title: 예상 대진표, Time: 42.91 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,11 +1,10 @@
 def solution(n,a,b):
     answer = 0
-    round = [i for i in range(n+1)]
     flag = True
     while flag:
         answer += 1
         for i in range(1,n+1,pow(2,answer)):
-            if a in round[i:i+pow(2,answer)] and b in round[i:i+pow(2,answer)]:
+            if i <= a < i+pow(2,answer) and i <= b < i+pow(2,answer):
                 flag = False
                 break
     return answer
