3481d2e6d7a26085820d66e9c73531a8a475e04d
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-04-14 16:30:36 +0900

    [unrated] Title: 귤 고르기, Time: 26.03 ms, Memory: 21.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,10 @@
+from collections import Counter
+def solution(k, tangerine):
+    answer = 0
+    for _, v in sorted(Counter(tangerine).items(), key = lambda x : x[1], reverse=True):
+        if k <= 0:
+            break
+        k -= v
+        answer += 1
+
+    return answer
