3d11ad3b41cff9d2fa2c719fd95035638a9c4282
voonkvuno <91922127+voonkvuno@users.noreply.github.com>
2023-04-19 18:51:12 +0900

    Delete 프로그래머스/unrated/176963. 추억 점수 directory

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++
@@ -1,6 +0,0 @@
-def solution(name, yearning, photo):
-    answer = []
-    dic = {a:b for a, b in zip(name, yearning)}
-    for p in photo:
-        answer.append(sum([dic[i] for i in p if i in dic.keys()]))
-    return answer
